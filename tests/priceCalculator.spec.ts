5aa8cd92852b8f5bf6b5398f5e6fc779a8fabc5e
unomauro <mauro.mora@unosquare.com>
2026-07-11 15:23:36 -0600

    failing tests RED

---
+++ tests/priceCalculator.spec.ts
@@ -0,0 +1,19 @@
+import {calculateTotal, calculateTax, CalculateGrandTotal} from '../utils/priceCalculator'
+import {test, expect} from '@playwright/test'
+
+
+test(('calculateTotal suma total'), () => {
+    expect(calculateTotal([29.99,9.99,15.99])).toBe(55.97)
+
+})
+
+
+test(('calculateTax regresa 8%'), () => {
+    expect(calculateTax(100)).toBe(8)
+
+})
+
+test(('calculateTotal suma con tax'), () => {
+    expect(CalculateGrandTotal([29.99,9.99,15.99])).toBe(60.45)
+
+})
